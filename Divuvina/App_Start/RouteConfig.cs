ebacd6a86f6d31fbb79b8c29a01cca0c14b944de
huunghia-divuvina <huunghia@divuvina.vn>
2016-10-16 15:11:38 +0700

    Nghia

--- Divuvina/App_Start/RouteConfig.cs
+++ Divuvina/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Divuvina
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "QuanLyXe", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DanhMucXe", action = "Index", id = UrlParameter.Optional }
             );
         }
 
